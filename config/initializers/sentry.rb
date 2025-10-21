1d3fab8353f5a6aae5f8ed08e3c145af9cf0270f
johnrieth <john@rieth.dev>
2025-10-21 11:03:18 -0700

    Match hero banner sizes between homepage and about page, fix Sentry for production only

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,26 +1,19 @@
+# config/initializers/sentry.rb
 Sentry.init do |config|
   config.dsn = 'https://a755aedf89849b5efdec57f24f189eff@o4510228619853824.ingest.us.sentry.io/4510228620836864'
-  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
-  # Add data like request headers and IP for users,
-  # see https://docs.sentry.io/platforms/ruby/data-management/data-collected/ for more info
-  config.send_default_pii = true
+  # Only enable in production and staging - NOT in development
+  config.enabled_environments = %w[production staging]
 
-  # Enable sending logs to Sentry
-  config.enable_logs = true
-  # Patch Ruby logger to forward logs
-  config.enabled_patches = [:logger]
+  # Only configure if enabled
+  if config.enabled_environments.include?(Rails.env)
+    config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+    config.send_default_pii = true
+    config.enable_logs = true
+    config.enabled_patches = [:logger]
 
-  # Set traces_sample_rate to 1.0 to capture 100%
-  # of transactions for tracing.
-  # We recommend adjusting this value in production.
-  config.traces_sample_rate = 1.0
-  # or
-  config.traces_sampler = lambda do |context|
-    true
+    # Lower sample rates for production (adjust based on traffic)
+    config.traces_sample_rate = 0.1  # 10% of requests
+    config.profiles_sample_rate = 0.1  # 10% of traces
   end
-  # Set profiles_sample_rate to profile 100%
-  # of sampled transactions.
-  # We recommend adjusting this value in production.
-  config.profiles_sample_rate = 1.0
 end
