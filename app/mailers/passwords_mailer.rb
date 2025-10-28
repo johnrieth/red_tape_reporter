873d931c7fd88a345044d5d9579fb1134173a2ff
johnrieth <john@rieth.dev>
2025-10-28 14:34:32 -0700

    fixing email reset

--- app/mailers/passwords_mailer.rb
+++ app/mailers/passwords_mailer.rb
@@ -1,6 +1,7 @@
 class PasswordsMailer < ApplicationMailer
   def reset(user)
     @user = user
+    @reset_url = edit_password_url(user.password_reset_token, host: "redtape.la", protocol: "https")
     mail subject: "Reset your password", to: user.email_address
   end
 end
