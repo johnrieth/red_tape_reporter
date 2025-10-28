82700f0cefc4ca066d0f8d06e922ec3ff50f255a
johnrieth <john@rieth.dev>
2025-10-28 14:11:46 -0700

    Fix verification email URLs by explicitly setting host and protocol

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -1,7 +1,7 @@
 class ReportMailer < ApplicationMailer
   def verification(report)
     @report = report
-    @verification_url = verify_report_url(token: report.verification_token)
+    @verification_url = verify_report_url(token: report.verification_token, host: "redtape.la", protocol: "https")
 
     mail(
       to: report.email,
